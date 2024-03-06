c87c9d43e521017b35a78d7191291987cb45716a
cmagarap <chrisjohn.agarap@gmail.com>
2024-03-06 15:20:11 +0800

    Added logging and changes in file paths

--- src/pandas/aggregate-visualize.py
+++ src/pandas/aggregate-visualize.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import sqlite3
 import plotly.express as px
+import os
+import logging
 
-conn = sqlite3.connect('taxidb.sqlite')
+logging.basicConfig(level=logging.INFO)
+
+conn = sqlite3.connect('src/pandas/taxidb.sqlite')
 
 trip_data = pd.read_sql('SELECT * FROM trip', conn)
 trip_data['tpep_pickup_datetime'] = pd.to_datetime(trip_data['tpep_pickup_datetime'], format='%Y-%m-%d %H:%M:%S')
@@ -13,11 +17,18 @@ filtered_data = filtered_data.sort_values(by='tpep_pickup_datetime')
 aggregate_data = filtered_data.groupby(filtered_data['tpep_pickup_datetime'].dt.date)['total_amount'].sum()
 aggregate_data = aggregate_data.to_frame()
 
-print(aggregate_data.info())
 aggregate_data.rename(columns={'total_amount': 'Amount', 'tpep_pickup_date': 'Date'}, inplace=True)
-print(aggregate_data.head(32))
-print(type(aggregate_data))
 
+logging.info('Generating Data Figure...')
 # Line Chart
 fig = px.line(aggregate_data, title='TCL Trip Total Amount Earned per day for December 2023', markers=True)
 fig.show()
+logging.info('Figure generated!')
+
+if not os.path.exists('figures'):
+    os.mkdir('figures')
+
+logging.info('Saving Data Figure as HTML file...')
+# Save the Figure into an HTML File
+fig.write_html('figures/line-fig1.html')
+logging.info('Figure saved!')
--- src/pandas/extract-load.py
+++ src/pandas/extract-load.py
@@ -4,12 +4,12 @@ import sqlite3
 
 logging.basicConfig(level=logging.INFO)
 
-raw_data = pd.read_parquet('../../dataset/yellow_tripdata_2023-12.parquet')
+raw_data = pd.read_parquet('dataset/yellow_tripdata_2023-12.parquet')
 filtered_data = raw_data.loc[raw_data['passenger_count'] > 0]
 
-conn = sqlite3.connect('taxidb.sqlite')
+conn = sqlite3.connect('src/pandas/taxidb.sqlite')
 
-with open('../../sql/sqlite-DDL.sql', 'r') as file:
+with open('sql/sqlite-DDL.sql', 'r') as file:
     query = file.read()
 
 conn.execute(query)
@@ -17,4 +17,5 @@ conn.execute(query)
 logging.info('WRITING DATA...')
 filtered_data.to_sql('trip', conn, if_exists='replace', index=False)
 conn.commit()
+logging.info('Data successfully loaded into MySQL')
 conn.close()
